70d183deb5002c2b3dc0675a226a8e2225db350d
Dieter Porth <info@mobger.de>
2021-04-05 15:57:22 +0200

    refactor versionnumber

--- ext_emconf.php
+++ ext_emconf.php
@@ -40,5 +40,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Dr. Dieter Piorth',
     'author_email' => 'info@mobger.de',
     'author_company' => 'Private',
-    'version' => '10.0.0',
+    'version' => '10.0.1',
 ];
